54cb54ad823dceb690d67642bd87a8b1d8cf4b9a
Jonathan Stoppani <jonathan@garetjax.info>
2009-08-22 19:10:04 +0200

    setup.py tweaking

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name='pyamqplib-trift',
+    name='amqplib_trift',
     version='0.1',
     description='Python library to use thrift services (client and servers) over AMQP with pyamqplib',
     author='Jonathan Stoppani',
-    author='Jonathan Stoppani',
     author_email='<jonathan.stoppani@edu.hefr.ch',
     url='http://github.com/garetjax/pyamqplib-thrift',
+    download_url='http://github.com/garetjax/pyamqplib-thrift/tarball/master'
     packages=['amqplib_thrift'],
     license='MIT',
     requires=('amqplib',),
-    provides=('pyamqplib-trift',),
+    provides=('amqplib_trift',),
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
