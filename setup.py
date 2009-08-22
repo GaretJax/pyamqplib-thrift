1c6ffa8b9f79eda84792e1f98b1c064a764a3836
Jonathan Stoppani <jonathan@garetjax.info>
2009-08-22 19:14:03 +0200

    setup.py tweaking

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.1',
     description='Python library to use thrift services (client and servers) over AMQP with pyamqplib',
     author='Jonathan Stoppani',
-    author_email='<jonathan.stoppani@edu.hefr.ch',
+    author_email='jonathan.stoppani@edu.hefr.ch',
     url='http://github.com/garetjax/pyamqplib-thrift',
-    download_url='http://github.com/garetjax/pyamqplib-thrift/tarball/master'
+    download_url='http://github.com/garetjax/pyamqplib-thrift/tarball/master',
     packages=['amqplib_thrift'],
     license='MIT',
     requires=('amqplib',),
